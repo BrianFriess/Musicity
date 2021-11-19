af0870d41f9d947985b953cf2b57e802d9aef75a
BrianFriess <brian.friesspro@gmail.com>
2021-11-19 11:42:44 +0100

    Initial Commit

---
+++ musicity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicity
+//
+//  Created by Brian Friess on 19/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
